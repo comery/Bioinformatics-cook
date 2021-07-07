e1a0caa8e38edd1c1ea90196d476dff741cd15b5
comery <comery@foxmail.com>
2021-07-07 15:11:59 +0800

    update

--- Perlscript/deal_genebank/draw_circos/auto_depth/outdir/random.depth.py
+++
@@ -1,7 +0,0 @@
-import random
-import sys
-with open(sys.argv[1], 'r') as fh:
-    for i in fh:
-        tmp = i.strip().split()
-        dep = random.randint(20, 100)
-        print("{0}\t{1}\t{2}\t{3}".format(tmp[0], tmp[1], tmp[2], dep))
--- Perlscript/deal_genebank/draw_circos/work.sh
+++
@@ -1,1 +0,0 @@
-perl draw_circos_for_mitogenome.pl -gb test1.gb -conf mitogenome.conf.txt
